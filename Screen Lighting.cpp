b4d666c7092b1221c8e1a62df032f30202912f1a
Romaan-Mahmood <161663578+Romaan-Mahmood@users.noreply.github.com>
2025-06-02 18:29:44 +0100

    Changed lighting res

---
+++ Screen Lighting.cpp
@@ -0,0 +1,82 @@
+#include "light source.cpp"
+#include <vector>
+#include "Maths.cpp"
+#include "camera.cpp"
+
+#include <list>
+#pragma once
+
+using namespace sf;
+using namespace std;
+
+class ScreenLighting {
+	list<RectangleShape*> rectangles;
+	float defaultTransparency;
+
+public:
+	ScreenLighting() {
+		int rDiv = 1;
+		for (int y = 0; y < 1080/rDiv; y++) {
+			for (int x = 0; x < 1920/rDiv; x++) {
+				RectangleShape* temp = new RectangleShape(Vector2f(rDiv,rDiv));
+				temp->setPosition(Vector2f(x*rDiv, y*rDiv));
+				rectangles.push_back(temp);
+			}
+		}
+
+		defaultTransparency = 200;
+	}
+
+	list<RectangleShape*> getRectangles() {
+		return rectangles;
+	}
+	void setDefaultTransparency(float t) {
+		defaultTransparency = t;
+	}
+	float getDefaultTransparency() {
+		return defaultTransparency;
+	}
+
+
+	void lightingCheck(LightSource* light, camera* cam) {
+		bool done = false;
+		for (RectangleShape* pixel : rectangles) {
+			Vector2f check = pixelPosition(pixel);
+			float distance = Maths::getDistance(pixelPosition(pixel), light->getPosition());
+			if (!done) {
+				done = true;
+			}
+
+			Color colour = light->getColour();
+			float red = colour.r;
+			float green = colour.g;
+			float blue = colour.b;
+
+			if (distance < light->getRange()) {
+
+
+				float percent = Maths::map(0, light->getRange(), 0, 100, light->getRange() - distance);
+
+				float newRed = Maths::map(0, 100, 0, red, percent);
+				float newGreen = Maths::map(0, 100, 0, green, percent);
+				float newBlue = Maths::map(0, 100, 0, blue, percent);
+				float newTransparency = 255 - Maths::map(0, 100, 0, light->getBrightness(), percent);
+				if (newTransparency > defaultTransparency) {
+					newTransparency = defaultTransparency;
+				}
+				pixel->setFillColor(Color(newRed, newGreen, newBlue, newTransparency));
+			}
+			else {
+				pixel->setFillColor(Color(0, 0, 0, defaultTransparency));
+			}
+
+
+		}
+	}
+
+	Vector2f pixelPosition(RectangleShape* pixel) {
+		Vector2f relPosition = pixel->getPosition();
+		Vector2f camPos = relPosition;
+		return (camPos);
+	}
+};
